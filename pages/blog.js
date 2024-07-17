a724102a79490868e52e96fda3e1238b03ad2c38
Mashal <Mashal.maqsood112@gmail.com>
2024-07-17 09:11:15 -0700

    Inital commit.

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+    <div>
+      This is my blog homepage
+    </div>
+  )
+}
+
+export default blog
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      This is my contact pagee
+    </div>
+  )
+}
+
+export default contact
